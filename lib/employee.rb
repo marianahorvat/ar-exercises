02da0a16d0bff44b1220272b498e534e82b95cb3
Mariana <mariana.horvat.qa@gmail.com>
2019-05-15 21:14:39 +0000

    Validations for both models

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,20 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_inclusion_of :hourly_rate, in: 40..200
+  validates_numericality_of :hourly_rate, only_integer: true
+  validates_associated :store
+
 end
+
+# Exercise 7: Validations for both models
+# Add validations to two models to enforce the following business rules:
+# Employees must always have a first name present
+# Employees must always have a last name present
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+
+# validates :hourly_rate:, inclusion: { in: 40..200 }, numericality: { only_integer: true }
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,17 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
+  validates :name, length: { minimum: 3, message: "3 characters is the maximum allowed" }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+
+  def must_carry
+      errors.add(:base, "There is no apparel for this store") if :mens_apparel == false && :womens_apparel == false
+  end
+
 end
+
+# Exercise 7: Validations for both models
+# Add validations to two models to enforce the following business rules:
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
