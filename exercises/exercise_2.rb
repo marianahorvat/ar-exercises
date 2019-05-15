04fa3810d8628571130e0b26e1c9085e46ef0d1a
Mariana <mariana.horvat.qa@gmail.com>
2019-05-15 18:33:36 +0000

    Update the first store instance in the database

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# returns the object for ID = 1:
+@store1 = Store.find(1)
+# returns the object for ID = 2:
+@store2= Store.find(2)
+
+# Updates one record
+@store1.update(name: 'Vaughan')
